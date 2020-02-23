61f6bd873670ee23a51b81546a88d001597fc03d
Ilya <static.ila@gmail.com>
2020-02-23 12:20:43 +0200

    Initial Commit

---
+++ ProblemSolver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProblemSolver
+//
+//  Created by Illia Ivanov on 2/23/20.
+//  Copyright © 2020 Illia Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
